a697b02269ea1fc237f1ddfdd804b9e40bfaa1e4
JuanDaniel00 <juanuribe@colegioguanenta.edu.co>
2024-05-16 21:06:50 -0500

    chore: update base href in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-base: process.env.NODE_ENV === 'production' ? 'inventarioDeCostos-vue-/' : '/'
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/inventarioDeCostos-vue-/' : '/',
   plugins: [vue()],
 })
 
